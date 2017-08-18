74048b5f19890a9af3bfcf784553fec19a9acad0
suppal <suppal>
2017-08-18 10:51:41 +0530

    Basic Website Template Without Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Page1Component } from './page1/page1.component';
+import { SubpageComponent } from './subpage/subpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Page1Component,
+    SubpageComponent
   ],
   imports: [
     BrowserModule
